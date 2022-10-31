455a20d29b14c49f4453eb855a9cfdde3ddf80e6
unknown <saradawsone@gmail.com>
2022-10-31 16:22:04 -0600

    tasks model functiongs set up

--- api/task/model.js
+++ api/task/model.js
@@ -1,1 +1,20 @@
-// build your `Task` model here
+const db = require('../../data/dbConfig');
+
+const getAll = () => {
+    return db('tasks')
+}
+
+const getById = (id) => {
+    return db('tasks').where('task_id', id).first()
+}
+
+const create = (task) => {
+    return db('tasks').insert(task)
+        .then(([id]) => getById(id))
+}
+
+module.exports = {
+    getAll,
+    getById,
+    create
+}
