f7e6dfd229204a9d485db7b365fa2091251283aa
unknown <saradawsone@gmail.com>
2022-10-31 14:23:13 -0600

    tables are initially created

---
+++ data/migrations/20221031201616_first-migration.js
@@ -0,0 +1,32 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = async function(knex) {
+    await knex.schema
+         .createTable('projects', table => {
+            table.increments('project_id')
+         })
+         .createTable('resources', table => {
+            table.increments('resource_id')
+         })
+         .createTable('tasks', table => {
+            table.increments('task_id')
+         })
+         .createTable('project_resources', table => {
+            table.increments('project_resource_id')
+         })
+
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = async function(knex) {
+    await knex.schema
+         .dropTableIfExists('project_resources')
+         .dropTableIfExists('tasks')
+         .dropTableIfExists('resources')
+         .dropTableIfExists('projects')
+};
